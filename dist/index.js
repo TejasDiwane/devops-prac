885d0141c40fcbdb7898370e8f1b03bb22519e17
TejasDiwane <tejasdiwane13@gmail.com>
2026-06-26 14:22:01 +0530

    ci-cd prac

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({
+        message: "first try"
+    });
+});
+app.listen(3000);
+//# sourceMappingURL=index.js.map
